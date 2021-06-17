f8ffd1d82798029a8efe35dd9bccb12b662d7a32
dawid <davoudseyedi@gmail.com>
2021-06-18 02:34:19 +0430

    create page componen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 // Modules
 import { AppRoutingModule } from './app-routing.module';
-import { PipeModule } from "./pipes/pipe.module";
+import { PipeModule } from './pipes/pipe.module';
 
 // Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ProductComponent } from './pages/product/product.component';
 
 // Services
-import { ApiService } from "./services/api.service";
-import { CustomInterceptor } from "./interceptor/custom.interceptor";
-import { LinkService } from "./services/link.service";
-import { MetaService } from "./services/meta.service";
-import { LocalStorageService } from "./services/local-storage.service";
-import { AuthService } from "./services/auth.service";
-import { HelpersService } from "./services/helpers.service";
+import { ApiService } from './services/api.service';
+import { CustomInterceptor } from './interceptor/custom.interceptor';
+import { LinkService } from './services/link.service';
+import { MetaService } from './services/meta.service';
+import { LocalStorageService } from './services/local-storage.service';
+import { AuthService } from './services/auth.service';
+import { HelpersService } from './services/helpers.service';
+import { SinglePodcastComponent } from './pages/single-podcast/single-podcast.component';
+import { PodcastsComponent } from './pages/podcasts/podcasts.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { SingleBlogComponent } from './pages/single-blog/single-blog.component';
 
 @NgModule({
     declarations: [
@@ -30,10 +34,14 @@ import { HelpersService } from "./services/helpers.service";
         HeaderComponent,
         FooterComponent,
         HomeComponent,
-        ProductComponent,
+        SinglePodcastComponent,
+        PodcastsComponent,
+        ContactComponent,
+        BlogComponent,
+        SingleBlogComponent,
     ],
     imports: [
-        BrowserModule.withServerTransition({ appId: "sample-app" }),
+        BrowserModule.withServerTransition({ appId: 'main-app' }),
         FormsModule,
         AppRoutingModule,
         HttpClientModule,
