3f1868ba8446108364d7dc7d8a5d021e31e913cc
dawid <davoudseyedi@gmail.com>
2021-07-31 22:20:41 +0430

    fix polyfills

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/sample-angular'),
+      dir: require('path').join(__dirname, './coverage/main-web-application'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- optimize_bundles.sh
+++ optimize_bundles.sh
@@ -6,7 +6,7 @@ EXT="html"
 HASH_CODE=$(echo $(date) | md5sum | awk '{print substr($1, 1, 8)}')
 head -n -2 "$FILE.$EXT" > "$FILE.tmp"
 echo "<script type='text/javascript' src='build.$HASH_CODE.js'></script></body></html>" >> "$FILE.tmp"
-cat runtime.*.js polyfills-es5.*.js polyfills.*.js scripts.*.js  main.*.js > build.$HASH_CODE.js
+cat runtime-es2015.*.js polyfills-es2015.*.js main-es2015.*.js scripts.*.js > build.$HASH_CODE.js
 cat "$FILE.tmp" > "$FILE.$EXT"
 echo '-----Bundles Concat finish-----'
 
