662f7ddc46c99301929bbaa66eddc32423f082f4
davoudseyedi <davoudseyedi@gmail.com>
2022-06-21 19:37:03 +0430

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  SITE_URL: 'http://localhost:300',
-  API_BASE_URL: 'http://localhost:3000/api',
+  SITE_URL: 'http://core-test.cafedeutsch.net:3000',
+  API_BASE_URL: 'http://core-test.cafedeutsch.net:3000/api',
 };
