b42bb4e7baf29d13d1c18a024922c242cbc9303d
davoudseyedi <davoudseyedi@gmail.com>
2022-06-21 18:58:04 +0430

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // SITE_URL: 'https://cafedeutsch.net',
-  // API_BASE_URL: 'https://core-test.cafedeutsch.net/api',
-  SITE_URL: 'http://localhost:3000',
-  API_BASE_URL: 'http://localhost:3000/api',
+  SITE_URL: 'https://cafedeutsch.net',
+  API_BASE_URL: 'https://core-test.cafedeutsch.net/api',
+  // SITE_URL: 'http://localhost:3000',
+  // API_BASE_URL: 'http://localhost:3000/api',
 };
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  // SITE_URL: 'https://cafedeutsch.net',
-  // API_BASE_URL: 'https://core-test.cafedeutsch.net/api'
   SITE_URL: 'http://localhost:3000',
   API_BASE_URL: 'http://localhost:3000/api',
 };
