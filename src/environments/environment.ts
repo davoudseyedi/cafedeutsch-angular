45e375dc1b900f349ad21fd8d5fb05b6acb12e79
davoudseyedi <davoudseyedi@gmail.com>
2022-06-21 19:07:49 +0430

    fix envirement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  SITE_URL: 'http://localhost:3000',
-  API_BASE_URL: 'http://localhost:3000/api',
+  SITE_URL: 'https://core-test.cafedeutsch.net',
+  API_BASE_URL: 'https://core-test.cafedeutsch.net/api',
 };
