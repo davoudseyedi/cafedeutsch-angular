badd91276e328e3bede2a482a8aac8a10ec9209c
davoudseyedi <davoudseyedi@gmail.com>
2022-06-29 17:46:12 +0430

    initial commit

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -27,7 +27,7 @@ export class ContactComponent implements OnInit {
   ];
 
   public background = '/assets/img/contact.jpg';
-  public subTitle = '                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز';
+  public subTitle = '';
 
   public webformModel = {
     name: '',
